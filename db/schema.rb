143eee2db91639143292ed19e14550857b84a1e7
Deven Gurung <deven9229@gmail.com>
2019-12-18 21:02:37 +0600

    added slug to all models

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_140421) do
+ActiveRecord::Schema.define(version: 2019_12_18_145916) do
 
   create_table "channels", force: :cascade do |t|
     t.string "channel"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "discussion_id"
+    t.string "slug"
   end
 
   create_table "discussions", force: :cascade do |t|
@@ -27,6 +28,18 @@ ActiveRecord::Schema.define(version: 2019_12_18_140421) do
     t.integer "user_id"
     t.integer "channel_di"
     t.integer "channel_id"
+    t.string "slug"
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "replies", force: :cascade do |t|
@@ -35,6 +48,7 @@ ActiveRecord::Schema.define(version: 2019_12_18_140421) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "discussion_id"
     t.integer "user_id"
+    t.string "slug"
   end
 
   create_table "roles", force: :cascade do |t|
