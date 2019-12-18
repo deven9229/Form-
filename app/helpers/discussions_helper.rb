5adafe1455fd26077c485be9dc1f29a6e8f63145
Deven Gurung <deven9229@gmail.com>
2019-12-18 20:52:55 +0600

    added admin roles

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -1,2 +1,10 @@
 module DiscussionsHelper
+
+  def discussion_author(discussion)
+   user_signed_in? && current_user.id == discussion.user.id
+  end
+
+  def discussion_author(reply)
+    user_signed_in? && current_user.id == reply.user_id
+  end
 end
