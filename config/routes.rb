90405fcbd193fd10bcfd8b47cd3638ae2291db27
Deven Gurung <deven9229@gmail.com>
2019-12-18 15:52:03 +0600

    defined controlles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :discussions
+  resources :channels
+  resources :discussions do
+    resources :replies
+  end
   root to: "home#index"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
